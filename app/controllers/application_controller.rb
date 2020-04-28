e97b7f854232c22208d69960859467cf4d9d3e05
Jamal Frank <jamaljfrank@gmail.com>
2020-04-28 14:20:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :authentication_check, :admin
+ 
+  def home
+  end
+
+  def authentication_check
+    if !logged_in?
+      redirect_to root_path
+    end
+  end
+
+  def logged_in?
+    session[:user_id]
+  end
+
+  def admin
+    current_user.admin
+  end 
+
+  def current_user
+    @user ||= User.find_by(id: session[:user_id])
+  end 
 end
