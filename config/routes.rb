f4c18a68ded3581553498ff460694e6065364df4
nicksolony <nicksolony@gmail.com>
2020-05-17 00:15:25 -0400

    taking a break

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only:[:new,:show, :create]
+  root 'application#home'
+  get 'logout' => 'sessions#destroy'
 end
